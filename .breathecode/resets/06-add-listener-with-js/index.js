6f80ad355478ba8797d18e0d405eb188f8ab4a46
Diego-EC <diegoezquerro@gmail.com>
2020-08-27 17:21:20 +0000

    Finished
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- .breathecode/resets/06-add-listener-with-js/index.js
+++ .breathecode/resets/06-add-listener-with-js/index.js
@@ -1,10 +1,6 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
-	document.getElementById("theGreen").addEventListener("click", listenerFunction);
 };
 
 //the listener function here
-function listenerFunction() {
-	alert("wuju");
-}
--- exercises/03-sum-values/index.js
+++ exercises/03-sum-values/index.js
@@ -1,8 +1,13 @@
-window.calculateSumListener = function calculateSumListener()
+window.calculateSumListener = function calculateSumListener() {
 	//Return the value of the input #firstNumber
 	var stringA = document.getElementById("firstNumber").value;
 	//Return the value of the input #secondNumber
 	var stringB = document.getElementById("secondNumber").value;
 
 	//your code goes here
+	let resultNumber = document.getElementById("resultNumber");
+	const intA = parseInt(stringA);
+	const intB = parseInt(stringB);
+	const sum = intA + intB;
+	resultNumber.value = sum;
 };
